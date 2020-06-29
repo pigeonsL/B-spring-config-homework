975b55f0f0c73abe6a9c95370f60792a485499ce
JingeLi <jinge.li@thoughtworks.com>
2020-06-29 14:17:05 +0800

    p2s2 finish

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,17 +13,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(properties = "levelNumber=2")
 class DemoSpringConfigApplicationTests {
 
 	@Autowired
 	private MockMvc mockMvc;
-	@Autowired
-	private LevelController levelController;
 
 	@Test
 	public void returnBasicWhenLevelNumberLesserThan1() throws Exception {
 
-		ReflectionTestUtils.setField(levelController, "levelNumber", 0);
 		String result = mockMvc.perform(MockMvcRequestBuilders.get("/level"))
 				.andReturn()
 				.getResponse()
@@ -34,7 +33,6 @@ class DemoSpringConfigApplicationTests {
 	@Test
 	public void returnBasicWhenLevelNumberEquals1() throws Exception {
 
-		ReflectionTestUtils.setField(levelController, "levelNumber", 1);
 		String result = mockMvc.perform(MockMvcRequestBuilders.get("/level"))
 				.andReturn()
 				.getResponse()
@@ -45,7 +43,6 @@ class DemoSpringConfigApplicationTests {
 	@Test
 	public void returnBasicWhenLevelNumberGreaterThan1() throws Exception {
 
-		ReflectionTestUtils.setField(levelController, "levelNumber", 2);
 		String result = mockMvc.perform(MockMvcRequestBuilders.get("/level"))
 				.andReturn()
 				.getResponse()
